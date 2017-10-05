1b85d9d5a2280386e36014ab4c392dea0d57c02c
Eljin013 <eligio013@gmail.com>
2017-10-04 17:49:17 -0700

    Updated Board and Game

--- 180aig/src/a1/Board.java
+++ 180aig/src/a1/Board.java
@@ -1,5 +1,25 @@
 package a1;
 
 public class Board {
-
+	private Board board;
+	private int[] y;
+	private char[] x;
+	
+	public Board() {
+		new Board(board);
+	}
+	
+	public Board(Board board) {
+		this.board = board;
+	}
+	
+	public void updateBoard() {
+		System.out.println("The board has been updated.");
+	}
+	
+	public void displayBoard() {
+		System.out.println("The board has been displayed.");
+	}
+	
+	
 }
--- 180aig/src/a1/Game.java
+++ 180aig/src/a1/Game.java
@@ -1,8 +1,15 @@
 package a1;
 
 public class Game {
-
+	private Board board;
+	
 	public Game() {
+		board = new Board();
 		
+	}
+	
+	public String askPlayerInput() {
+		
+		return "Player input.";
 	}
 }
